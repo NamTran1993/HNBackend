0525fe3fba13462393be2fd44167c66092c4f348
Nam Tran <tranhuynam93@gmail.com>
2021-11-30 10:37:19 +0700

    NamTran[2021-11-30]__ADD

---
+++ Module/TCaptureScreen/Readme.txt
@@ -0,0 +1,10 @@
+﻿-- Hướng dẫn call Functions
+ 1. Khi start con .exe
+       THookKeyBoard.StartWithOS(start_app);
+       THookKeyBoard.HideWindow();
+ 2. Viet Luong CaptureScreen
+ // .......
+ 3.
+        THookKeyBoard.HookKeyboard();
+        System.Windows.Forms.Application.Run();
+        THookKeyBoard.WindowsHook();
---
+++ Module/TCaptureScreen/TCaptureMain.cs
@@ -0,0 +1,45 @@
+﻿using HNBackend.Global;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HNBackend.Module.TCaptureScreen
+{
+    public class TCaptureMain
+    {
+        public static void CaptureImage(string folderSaveImage, string fileName = "", string extension = "")
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(folderSaveImage))
+                    throw new Exception("Folder Save Image IsNullOrEmpty.");
+
+                Bitmap bmScreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+                var gfxScreenshot = Graphics.FromImage(bmScreen);
+                gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+
+                if (string.IsNullOrEmpty(extension))
+                    extension = ".png";
+
+                string full_path = string.Empty;
+                if (string.IsNullOrEmpty(fileName))
+                    fileName = string.Format("{0}_{1}{2}", "ScreenShot", TGlobal.CreateGUID(TGUID.TIME), extension);
+
+                if (!Directory.Exists(folderSaveImage))
+                    Directory.CreateDirectory(folderSaveImage);
+
+                full_path = string.Format("{0}\\{1}", folderSaveImage, fileName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
